baa3f64be68bd3b285db85bf6ba932727115f712
AdaboyOP20 <85760909+AdaboyOP20@users.noreply.github.com>
2023-03-10 16:15:37 +0530

    Add files via upload

---
+++ misc/app.js
@@ -0,0 +1,17 @@
+
+const userName = userName.value;
+const age = age.value;
+const sports = sports.value;
+const subjects = subjects.value;
+const friends = friends.value;
+const school = school.value;
+
+function story()
+{
+    window.location = "StoryResult.html";
+}
+
+function writeStory()
+{
+    StoryContainer.innerHTML = "there was a boy named";
+}
